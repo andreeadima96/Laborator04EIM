829ca7ed105b38b32f8bd8c09678578bebb57b86
andreeadima96 <andreeaadima@gmail.com>
2019-03-24 08:06:48 -0700

    implemented tasks for laboratory 4

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/Constants.java
@@ -0,0 +1,6 @@
+package lab03.eim.systems.cs.pub.ro;
+
+public interface Constants {
+    final public static int PERMISSION_REQUEST_CALL_PHONE = 1;
+    final public static int CONTACTS_MANAGER_REQUEST_CODE = 2017;
+}
---
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/ContactsManagerActivity.java
@@ -0,0 +1,140 @@
+package lab03.eim.systems.cs.pub.ro;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ContactsManagerActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText phoneEditText;
+    private EditText emailEditText;
+    private EditText addressEditText;
+    private EditText jobTitleEditText;
+    private EditText companyEditText;
+    private EditText websiteEditText;
+    private EditText imEditText;
+
+    private Button showHideAdditionalFieldsButton;
+    private Button saveButton;
+    private Button cancelButton;
+    private LinearLayout additionalFieldsContainer;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.fields_button:
+                    switch (additionalFieldsContainer.getVisibility()){
+                        case View.VISIBLE:
+                            showHideAdditionalFieldsButton.setText(R.string.show_additional_fields);
+                            additionalFieldsContainer.setVisibility(View.INVISIBLE);
+                            break;
+                        case View.INVISIBLE:
+                            showHideAdditionalFieldsButton.setText(getString(R.string.hide_additional_fields));
+                            additionalFieldsContainer.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                    break;
+                case R.id.save_button:
+                    String name = nameEditText.getText().toString();
+                    String phone = phoneEditText.getText().toString();
+                    String email = emailEditText.getText().toString();
+                    String address = addressEditText.getText().toString();
+                    String jobTitle = jobTitleEditText.getText().toString();
+                    String company = companyEditText.getText().toString();
+                    String website = websiteEditText.getText().toString();
+                    String im = imEditText.getText().toString();
+
+
+                    Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                    intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                    if (name != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
+                    }
+                    if (phone != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.PHONE, phone);
+                    }
+                    if (email != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+                    }
+                    if (address != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.POSTAL, address);
+                    }
+                    if (jobTitle != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.JOB_TITLE, jobTitle);
+                    }
+                    if (company != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.COMPANY, company);
+                    }
+                    ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
+                    if (website != null) {
+                        ContentValues websiteRow = new ContentValues();
+                        websiteRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
+                        websiteRow.put(ContactsContract.CommonDataKinds.Website.URL, website);
+                        contactData.add(websiteRow);
+                    }
+                    if (im != null) {
+                        ContentValues imRow = new ContentValues();
+                        imRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
+                        imRow.put(ContactsContract.CommonDataKinds.Im.DATA, im);
+                        contactData.add(imRow);
+                    }
+//                    intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
+//                    startActivity(intent);
+                    startActivityForResult(intent, Constants.CONTACTS_MANAGER_REQUEST_CODE);
+                    break;
+                case R.id.cancel_button:
+                    setResult(Activity.RESULT_CANCELED, new Intent());
+                    finish();
+                    break;
+            }
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_manager);
+
+       showHideAdditionalFieldsButton = findViewById(R.id.fields_button);
+       showHideAdditionalFieldsButton.setOnClickListener(buttonClickListener);
+       saveButton = findViewById(R.id.save_button);
+       saveButton.setOnClickListener(buttonClickListener);
+       cancelButton = findViewById(R.id.cancel_button);
+       cancelButton.setOnClickListener(buttonClickListener);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String phone = intent.getStringExtra("ro.pub.cs.systems.eim.lab04.contactsmanager.PHONE_NUMBER_KEY");
+            if (phone != null) {
+                phoneEditText.setText(phone);
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.phone_error), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        switch(requestCode) {
+            case Constants.CONTACTS_MANAGER_REQUEST_CODE:
+                setResult(resultCode, new Intent());
+                finish();
+                break;
+        }
+    }
+}
